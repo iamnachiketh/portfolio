59e8023e3d5c033f2d5a14cf18599c54d2d2b38f
Nachiketha <nachiketh.k.c@brainsightai.com>
2023-10-13 18:43:05 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {motion} from "framer-motion"
+import {styles} from "../styles"
+import {ComputersCanvas} from "./canvas"
+const Hero = () => {
+  return (
+    <section className='relative w-full 
+    h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute 
+      inset-0 top-[120px] max-w-7xl max-auto 
+      flex flex-row items-start gap-5`}>
+        <div className='flex flex-col 
+        justify-center items-center mt-5'>
+          <div className='rounded-full w-5 
+          h-5 bg-[#915eff]'/>
+          <div className='w-1 h-40 violet-gradient sm:h-80'/>
+        </div>
+        <div>
+        <h1 className={`${styles.heroHeadText} text-white`}>Hi I'm <span className='text-[#951eff]
+         hover:text-white'>Nachiketh</span></h1>
+         <p className={`${styles.heroSubText} 
+         mt-2 text-white-100`}>
+           I am a Full Stack developer I develop  
+           <br className='sm:block hidden'/>web applications, Backend applications
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas/>
+      <div className='absolute xs:bottom-10 bottom-32 
+      flex w-full justify-center items-center'>
+       <a href='#about'>
+          <div className='w-[35px] h-[64px] 
+          rounded-3xl border-4 border-secondary 
+          flex justify-center items-start p2'>
+            <motion.dev
+            animate={{
+              y:[0,26,0]
+            }}
+            transition={{
+              duration:1.5,
+              repeat:Infinity,
+              repeatType:'loop'
+            }}
+            className='w-3 h-3 rounded-full 
+            bg-secondary mb-1'
+            />
+          </div>
+       </a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,67 @@
+import React,{useEffect,useState,Suspense} from 'react'
+import {Canvas} from "@react-three/fiber"
+import {useGLTF,Preload,OrbitControls} from "@react-three/drei";
+import CanvasLoader from "../Loader";
+const Computers = ({isMobile}) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf');
+  return (
+    <mesh>
+      <hemisphereLight intensity={3} 
+      groundColor="black"/>
+      <pointLight intensity={2.5}/>
+      <spotLight position={[-20,50,10]}
+      angle={0.12}
+      penumbra={1}
+      castShadow
+      intensity={1}
+      shadow-mapSize={1024}
+      />
+      <primitive 
+      object={computer.scene}
+      position={isMobile? [0,-3,-2.2] : [0,-3.45,-1.5]}
+      scale={isMobile? 0.7 : 0.75}
+      rotation={[-0.01,-0.2,-0.1]}
+      />
+    </mesh>
+  )
+}
+
+const ComputersCanvas = ()=>{
+  const [isMobile,setIsMobile] = useState(false);
+  useEffect(()=>{
+    //Add an event listener that changes to the size of the device  
+    const mediaQuery = window.matchMedia('(max-width:600px)');
+    // set the intial value of isMobile state variable 
+      setIsMobile(mediaQuery.matches);
+    // define a call back function to handle changes to the media query
+      const handleMediaQueryChanges = (e)=>{
+          setIsMobile(e.matches)
+      }
+    // Add callback function as a listener for the changes to the media query
+      mediaQuery.addEventListener('change',handleMediaQueryChanges);
+    // remove the listener when the component unmounted
+      return ()=>{
+         mediaQuery.removeEventListener('change',handleMediaQueryChanges);
+      }
+  },[])
+  return(
+     <Canvas
+     frameloop="demand"
+     shadows
+     camera={{position:[20,3,5],fov:25}}
+     gl={{preserveDrawingBuffer:true}}
+     >
+     <Suspense fallback={<CanvasLoader/>}>
+       <OrbitControls 
+       enableZoom={false}
+       maxPolarAngle={Math.PI/2}
+       minPolarAngle={Math.PI/2}
+       />
+       <Computers isMobile={isMobile}/>
+       </Suspense>
+     <Preload all/>
+     </Canvas>
+  )
+}
+
+export default ComputersCanvas
